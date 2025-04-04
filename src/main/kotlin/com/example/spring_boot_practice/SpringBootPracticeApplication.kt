7a16e96a83866f7b77bb23188ab2af96460cd894
PratikSherdiwala <sherdiwala.pratik@ibm.com>
2025-04-04 10:22:15 +0530

    Initial commit

---
+++ src/main/kotlin/com/example/spring_boot_practice/SpringBootPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.spring_boot_practice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootPracticeApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootPracticeApplication>(*args)
+}
